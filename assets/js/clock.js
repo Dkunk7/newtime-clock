f0cba12739b5ca158af3920fbc830f4688dff0b9
Dylan K <drkunkel7@gmail.com>
2023-04-19 13:39:57 -0500

    Not hideous now. Not pretty either

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -22,6 +22,13 @@ I'm gonna need new day names
 
 New month names
     Primary, Secondary, Terch, Quatal, Quintay, Senune, Septuly, Octust, Nonember, Denember 
+
+
+Add buttons to change format: 
+    MM/dd/yyy hh:mm:ss    08/05/2006 03:05:15 PM
+    M/d/yy h :m:s tt	  8/5/06 3:5:15 PM
+    ddd MMM dd yyyy	      Sat Aug 05 2006
+    dddd, MMMM dd yyyy	  Saturday, August 05 2006
 */
 
 function currentTime() {
@@ -62,63 +69,63 @@ function currentTime() {
 function newTime() {
     let date = new Date();
     let hh = date.getHours();
-    console.log(hh + " hours")
+    // console.log(hh + " hours")
     let mm = date.getMinutes();
     let ss = date.getSeconds();
-    let session = "AM";
+    // let session = "AM";
 
-    let DD = date.getDate();
-    let MM = date.getMonth();
+    // let DD = date.getDate();
+    // let MM = date.getMonth();
     let YY = date.getFullYear();
 
     // Number of leap years since year 0
     let leapYearCount = Math.floor(YY / 4) // This divides the current year by 4 and rounds down to calculate the number of leap years
-    console.log(leapYearCount + " leap count");
+    // console.log(leapYearCount + " leap count");
 
     // Number of normal years since year 0
     let normalYearCount = YY - leapYearCount;
-    console.log(normalYearCount + " normal count");
+    // console.log(normalYearCount + " normal count");
 
     // Current number of days gone by this year
     let start = new Date(date.getFullYear(), 0, 0); // Gets beginning of the year
     let diff = date - start; // Subtract start of the year from todays date
-    console.log(diff + " diff")
+    // console.log(diff + " diff")
     let dayCount = Math.floor(diff / normalDailySeconds); // Round down and divide by seconds in a day
-    console.log(dayCount + " day count")
+    // console.log(dayCount + " day count")
 
     // Current number of hours today is the same as hh above
     // Same for minutes and seconds
 
     let totalSeconds = (normalYearCount * yearlySeconds) + (leapYearCount * leapyearlySeconds) + (dayCount * 86400 /* Seconds in a day */) + (hh * 3600 /* Seconds in an hour*/) + (mm * 60 /* Seconds in minute */) + ss;
-    console.log(totalSeconds + " total")
+    // console.log(totalSeconds + " total")
 
     // Start of conversion to new time from total seconds
     let ntYear = Math.floor(totalSeconds / 1000000); // total seconds divided by seconds in a newtime year
     let yearRemainder = totalSeconds % 1000000; // gets the seconds remaining
-    console.log(ntYear + " nt year")
-    console.log(yearRemainder + " y remainder")
+    // console.log(ntYear + " nt year")
+    // console.log(yearRemainder + " y remainder")
 
     let ntMonth = Math.floor(yearRemainder / 100000); // seconds this year divided by seconds in a month
     let monthRemainder = yearRemainder % 100000; // seconds remaining
-    console.log(ntMonth + " nt month");
-    console.log(monthRemainder + " m remainder")
+    // console.log(ntMonth + " nt month");
+    // console.log(monthRemainder + " m remainder")
     
     // Skipping week because we don't use that
 
     let ntDay = Math.floor(monthRemainder / 1000); // seconds this month divided by seconds in a day
     let dayRemainder = monthRemainder % 1000; // seconds remaining
-    console.log(ntDay + " nt day");
-    console.log(dayRemainder + " d remainder");
+    // console.log(ntDay + " nt day");
+    // console.log(dayRemainder + " d remainder");
 
     let ntHour = Math.floor(dayRemainder / 100); // seconds today divided by seconds in an hour
     let hourRemainder = dayRemainder % 100; // seconds remaining
-    console.log(ntHour + " nt hour");
-    console.log(hourRemainder + " h remainder");
+    // console.log(ntHour + " nt hour");
+    // console.log(hourRemainder + " h remainder");
 
     let ntMin = Math.floor(hourRemainder / 10); // seconds this hour divided by seconds in a minute
     let ntSec = hourRemainder % 10; // Minute remainder is the same as seconds
-    console.log(ntMin + " nt min");
-    console.log(ntSec + " nt sec")
+    // console.log(ntMin + " nt min");
+    // console.log(ntSec + " nt sec")
 
 
 
